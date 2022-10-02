5a4aaff796a06990f24a3c64ceb2b867f23c69fd
slastnikova <slastnikova.natalia@mail.ru>
2022-10-02 14:36:32 +0300

    second commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = new string[8] {"579", "dog", "hello", "final", "projeck", "yes", "No", "18"};
+string[] array2 = new string[array1.Length];
+void SecondArrayOfThree(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayOfThree(array1, array2);
+PrintArray(array2);
